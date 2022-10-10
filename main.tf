c13f7d4c3520e0c3b68e46a39d3b256fdd174fbd
RaviAnand Mohabir <moravrav@gmail.com>
2022-10-10 12:14:50 +0200

    chore: :pushpin: allow Kubernetes provider 2.0.0 or higher

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "2.13.1"
+      version = ">= 2.0.0"
     }
   }
   experiments = [module_variable_optional_attrs]
